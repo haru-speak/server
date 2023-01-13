5a7e689b05fc383076b244b7ca735ca829c9fb8e
nathan29849 <phs960418@gmail.com>
2023-01-14 00:59:07 +0900

    coomon: [#3] Swagger 적용을 위한 의존성 추가 및 Config 클래스 작성

---
+++ be/src/main/java/com/example/be/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.be.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.OAS_30)
+			.useDefaultResponseMessages(false)
+			.groupName("haru-speak")
+			.apiInfo(apiInfo())
+			.select()
+			.apis(RequestHandlerSelectors
+				.basePackage("com.example.be"))
+			.paths(PathSelectors.any())
+			.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+			.title("하루 스픽 API")
+			.description("API 상세소개 및 사용법 등")
+			.version("1.0")
+			.build();
+	}
+}
