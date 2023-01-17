6114d3bc43b5478acdf2a69339e799be59279229
nathan29849 <phs960418@gmail.com>
2023-01-17 18:57:30 +0900

    common: [#4] package 구조 적용

--- be/src/main/java/com/example/be/common/config/SwaggerConfig.java
+++ be/src/main/java/com/example/be/common/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.be.config;
+package com.example.be.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
