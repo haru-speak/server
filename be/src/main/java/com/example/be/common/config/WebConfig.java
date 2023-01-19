9f591a5c3518521234305429b4e73d2e67bfe77a
나든 <67811880+nathan29849@users.noreply.github.com>
2023-01-20 03:11:39 +0900

    도메인 공통 정보를 담는 Base Entity 생성 (#17)
    * feat: [#10] BaseEntity 생성
    
    * feat: [#10] JPA Auditing 설정

--- be/src/main/java/com/example/be/common/config/WebConfig.java
+++ be/src/main/java/com/example/be/common/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.example.be.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
