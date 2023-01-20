4ad90cece777b59b993b7851f27210f02738f6c4
YeonSang <leeys12181811@gmail.com>
2023-01-20 10:57:48 +0900

    feat: [#13] 수정사항 적용 완료

--- be/src/main/java/com/example/be/core/application/dto/response/SpeakingLogDetailResponse.java
+++ be/src/main/java/com/example/be/core/application/dto/response/SpeakingLogDetailResponse.java
@@ -11,29 +11,24 @@ import java.util.List;
 @Getter
 public class SpeakingLogDetailResponse {
 
-	@Schema(type = "Long",
-			description = "멤버 ID, NOT NULL")
+	@Schema(type = "Long", description = "멤버 ID, NOT NULL")
 	@NotNull
 	@Positive
 	private final Long memberId;
 
-	@Schema(type = "String",
-			description = "제목, NOT NULL")
+	@Schema(type = "String", description = "제목, NOT NULL")
 	@NotBlank
 	private final String title;
 
-	@Schema(type = "String",
-			description = "음성 녹음 URL, NOT NULL")
+	@Schema(type = "String", description = "음성 녹음 URL, NOT NULL")
 	@NotBlank
 	private final String voiceRecord;
 
-	@Schema(type = "String",
-			description = "음성 텍스트 URL, NOT NULL")
+	@Schema(type = "String", description = "음성 텍스트 URL, NOT NULL")
 	@NotBlank
 	private final String voiceText;
 
-	@Schema(type = "Int",
-			description = "좋아요 개수, NOT NULL")
+	@Schema(type = "Int", description = "좋아요 개수, NOT NULL")
 	@NotNull
 	private final Integer likeCount;
 
@@ -44,6 +39,7 @@ public class SpeakingLogDetailResponse {
 	@Schema(type = "Boolean", description = "좋아요 여부, NOT NULL")
 	@NotNull
 	private final Boolean isLiked;
+
 	@Schema(type = "List<Comment>", description = "댓글 리스트, NULLABLE")
 	private final List<CommentResponse> comments;
 
