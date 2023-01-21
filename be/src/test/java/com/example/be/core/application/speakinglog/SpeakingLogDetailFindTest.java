dde6404fd6462ddff9c4381967dea94ace0df04f
nathan29849 <phs960418@gmail.com>
2023-01-22 02:46:15 +0900

    refactor: [#37] InitServiceTest 추가로 인한 스피킹 로그 상세 조회 테스트 리팩토링

--- be/src/test/java/com/example/be/core/application/speakinglog/SpeakingLogDetailFindTest.java
+++ be/src/test/java/com/example/be/core/application/speakinglog/SpeakingLogDetailFindTest.java
@@ -2,47 +2,18 @@ package com.example.be.core.application.speakinglog;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.be.core.application.InitServiceTest;
 import com.example.be.core.application.SpeakingLogService;
 import com.example.be.core.application.dto.response.SpeakingLogDetailResponse;
-import com.example.be.core.domain.member.Member;
-import com.example.be.core.domain.speakinglog.SpeakingLog;
-import com.example.be.core.repository.member.MemberRespository;
-import com.example.be.core.repository.speakinglog.SpeakingLogRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest
 @DisplayName("서비스 테스트 : SpeakingLog 상세 조회")
-public class SpeakingLogDetailFindTest {
-
+public class SpeakingLogDetailFindTest extends InitServiceTest {
 	@Autowired
 	private SpeakingLogService speakingLogService;
-
-	@Autowired
-	private SpeakingLogRepository speakingLogRepository;
-
-	@Autowired
-	private MemberRespository memberRespository;
-
-	@BeforeEach
-	void init() {
-		Member loginMember = new Member("nathan", "nathan1234@google.com", "asdf1234@");
-		memberRespository.save(loginMember);
-		SpeakingLog speakingLog = new SpeakingLog(
-			loginMember,
-			"첫 번째 스피킹 로그",
-			"dummy-voice-record-data",
-			"dummy-voice-text-data"
-		);
-		speakingLogRepository.save(speakingLog);
-	}
 
 	@Nested
 	@DisplayName("스피킹 로그를 상세 조회할 때")
@@ -67,9 +38,9 @@ public class SpeakingLogDetailFindTest {
 				assertThat(response.getMemberId()).isEqualTo(1L);
 				assertThat(response.getIsLiked()).isFalse();
 				assertThat(response.getLikeCount()).isZero();
-				assertThat(response.getTitle()).isEqualTo("첫 번째 스피킹 로그");
-				assertThat(response.getVoiceRecord()).isEqualTo("dummy-voice-record-data");
-				assertThat(response.getVoiceText()).isEqualTo("dummy-voice-text-data");
+				assertThat(response.getTitle()).isEqualTo("speaking-log-title11");
+				assertThat(response.getVoiceRecord()).isEqualTo("dummy-voice-record11");
+				assertThat(response.getVoiceText()).isEqualTo("dummy-voice-text11");
 			}
 		}
 	}
