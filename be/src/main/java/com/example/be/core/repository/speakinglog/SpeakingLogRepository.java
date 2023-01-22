ee0f8a0e50548bcca169836575e7da3139fff077
DoooongDong <rhkswls98@kakao.com>
2023-01-23 01:59:44 +0900

    feat: [#36] 스피킹 로그 삭제 기능 구현

--- be/src/main/java/com/example/be/core/repository/speakinglog/SpeakingLogRepository.java
+++ be/src/main/java/com/example/be/core/repository/speakinglog/SpeakingLogRepository.java
@@ -1,8 +1,6 @@
 package com.example.be.core.repository.speakinglog;
 
-import com.example.be.core.application.dto.request.SpeakingLogConditionRequest;
 import com.example.be.core.domain.speakinglog.SpeakingLog;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
