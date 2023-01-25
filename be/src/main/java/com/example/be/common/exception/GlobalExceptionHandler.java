4de832092cd8bea369688644910fa23b9e804482
nathan29849 <phs960418@gmail.com>
2023-01-25 17:37:11 +0900

    refactor: [#46] SpeakingLog 데이터 생성시 voiceRecord가 https:~ 형식을 취하도록 변경

--- be/src/main/java/com/example/be/common/exception/GlobalExceptionHandler.java
+++ be/src/main/java/com/example/be/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.example.be.common.exception;
 
+import static com.example.be.common.exception.ErrorCodeAndMessages.BAD_REQUEST_ERROR;
+
 import com.example.be.common.response.BaseResponse;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,10 +14,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	/**
+	 * 400 BAD REQUEST Spring Validation
+	 */
+	@ExceptionHandler(BindException.class)
+	private BaseResponse<Void> handleBadRequest(BindException e) {
+		String errorMessage = e.getBindingResult()
+			.getFieldErrors()
+			.stream()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.collect(Collectors.joining("\n"));
+		log.error("BindException: {}", errorMessage);
+		return BaseResponse.error(BAD_REQUEST_ERROR.getCode(), errorMessage);
+	}
+
 	@ExceptionHandler(BaseException.class)
-	public BaseResponse<String> handleConversionFailed(BaseException e) {
-		log.info("Error Code={}, Error Message={}", e.getCode(), e.getMessage());
-		return new BaseResponse<>(e.getCode(), e.getMessage(), null);
+	public BaseResponse<Void> handleConversionFailed(BaseException e) {
+		log.warn("Error Code={}, Error Message={}", e.getCode(), e.getMessage());
+		return BaseResponse.error(e.getCode(), e.getMessage());
 	}
 
 }
