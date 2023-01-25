759c02eded3d0abba88a7396533f09be24d4732c
DoooongDong <rhkswls98@kakao.com>
2023-01-25 13:45:22 +0900

    infra: [#42] AWS S3 gradle 의존성 추가

--- be/src/main/java/com/example/be/BeApplication.java
+++ be/src/main/java/com/example/be/BeApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeApplication {
 
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
 	}
