a0d08b668806cf08058b697efdacb7e375a6f50b
nathan29849 <phs960418@gmail.com>
2023-01-26 17:32:08 +0900

    refactor: [#46] Response DTO 필드 모두 final 키워드 사용 및 기본 생성자 제거

--- be/src/main/java/com/example/be/core/application/dto/response/SpeakingLogsResponse.java
+++ be/src/main/java/com/example/be/core/application/dto/response/SpeakingLogsResponse.java
@@ -3,24 +3,19 @@ package com.example.be.core.application.dto.response;
 import com.example.be.core.domain.speakinglog.SpeakingLogType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
-import lombok.AccessLevel;
+import java.util.List;
 import lombok.Getter;
 
-import javax.validation.constraints.NotNull;
-import java.util.List;
-import lombok.NoArgsConstructor;
-
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SpeakingLogsResponse {
 
 	@Schema(enumAsRef = true, description = "스피킹 로그 조회 타입")
-	private SpeakingLogType type;
+	private final SpeakingLogType type;
 
 	@Schema(type = "date", description = "스피킹 로그 조회 날짜")
-	private LocalDate date;
+	private final LocalDate date;
 
-	private List<SpeakingLogResponse> speakingLogsResponse;
+	private final List<SpeakingLogResponse> speakingLogsResponse;
 
 	public SpeakingLogsResponse(SpeakingLogType type, LocalDate date,
 		List<SpeakingLogResponse> speakingLogsResponse) {
