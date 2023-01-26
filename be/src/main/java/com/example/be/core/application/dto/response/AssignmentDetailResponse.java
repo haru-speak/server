3eb979dab7a578f347a8aa5d649952acf27ed7c9
YeonSang <leeys12181811@gmail.com>
2023-01-26 20:33:07 +0900

    각 Response에서 검증 어노테이션, 기본생성자 제거 및 final 키워드 추가

--- be/src/main/java/com/example/be/core/application/dto/response/AssignmentDetailResponse.java
+++ be/src/main/java/com/example/be/core/application/dto/response/AssignmentDetailResponse.java
@@ -2,34 +2,25 @@ package com.example.be.core.application.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
-import javax.validation.constraints.NotBlank;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AssignmentDetailResponse {
 
   @Schema(type = "Long", description = "과제 ID, NOT NULL")
-  @NotBlank
-  private Long assignmentId;
+  private final Long assignmentId;
 
   @Schema(type = "String", description = "제목, NOT NULL")
-  @NotBlank
-  private String title;
+  private final String title;
 
   @Schema(type = "String", description = "내용, NOT NULL")
-  @NotBlank
-  private String content;
+  private final String content;
 
   @Schema(type = "date", description = "제출 기한, NOT NULL")
-  @NotBlank
-  private LocalDate deadline;
+  private final LocalDate deadline;
 
   @Schema(type = "String", description = "음성 기록 URL, NOT NULL")
-  @NotBlank
-  private String voiceRecord;
+  private final String voiceRecord;
 
   public AssignmentDetailResponse(Long assignmentId, String title, String content,
       LocalDate deadline, String voiceRecord) {
--- be/src/main/java/com/example/be/core/application/dto/response/AssignmentResponse.java
+++ be/src/main/java/com/example/be/core/application/dto/response/AssignmentResponse.java
@@ -2,34 +2,25 @@ package com.example.be.core.application.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
-import javax.validation.constraints.NotBlank;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AssignmentResponse {
 
   @Schema(type = "Long", description = "과제 ID, NOT NULL")
-  @NotBlank
-  private Long assignmentId;
+  private final Long assignmentId;
 
   @Schema(type = "String", description = "과제 제목, NOT NULL")
-  @NotBlank
-  private String assignmentTitle;
+  private final String assignmentTitle;
 
   @Schema(type = "Long", description = "스터디 ID, NOT NULL")
-  @NotBlank
-  private Long studyId;
+  private final Long studyId;
 
   @Schema(type = "String", description = "스터디 제목, NOT NULL")
-  @NotBlank
-  private String studyTitle;
+  private final String studyTitle;
 
   @Schema(type = "date", description = "제출 기한, NOT NULL")
-  @NotBlank
-  private LocalDate deadline;
+  private final LocalDate deadline;
 
   public AssignmentResponse(Long assignmentId, String assignmentTitle, Long studyId,
       String studyTitle, LocalDate deadline) {
