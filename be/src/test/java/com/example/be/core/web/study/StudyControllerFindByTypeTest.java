74d5dbe4ff8d2d7f9ea53e9f5f149010a2eed893
YeonSang <leeys12181811@gmail.com>
2023-01-27 15:06:02 +0900

    test: [#55] 스터디 타입 조회 테스트에 타입 없이 조회 했을 때 테스트 추가

--- be/src/test/java/com/example/be/core/web/study/StudyControllerFindByTypeTest.java
+++ be/src/test/java/com/example/be/core/web/study/StudyControllerFindByTypeTest.java
@@ -2,6 +2,7 @@ package com.example.be.core.web.study;
 
 import static com.example.be.common.response.ResponseCodeAndMessages.FIND_STUDY_SUCCESS;
 import static com.example.be.core.domain.study.StudyType.ALL;
+import static com.example.be.core.domain.study.StudyType.MY;
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -42,6 +43,30 @@ class StudyControllerFindByTypeTest extends InitStudyControllerTest{
 
         //when
         ResultActions resultActions = mockMvc.perform(get("/study?type=all")
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .contentType(MediaType.APPLICATION_JSON_VALUE));
+
+        //then
+        resultActions.andExpect(status().isOk())
+            .andExpect(content().string(objectMapper.writeValueAsString(baseResponse)))
+            .andDo(print());
+
+        verify(studyService).find(refEq(studyConditionRequest));
+      }
+
+      @Test
+      @DisplayName("타입 없이 스터디 조회시 MY TYPE 스터디가 조회된다")
+      void find_no_type_study() throws Exception {
+        //given
+        StudyConditionRequest studyConditionRequest = new StudyConditionRequest(null);
+        StudiesResponse studiesResponse = new StudiesResponse(MY, null);
+        BaseResponse<StudiesResponse> baseResponse = new BaseResponse<>(FIND_STUDY_SUCCESS, studiesResponse);
+
+        when(studyService.find(refEq(studyConditionRequest)))
+            .thenReturn(studiesResponse);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get("/study")
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .contentType(MediaType.APPLICATION_JSON_VALUE));
 
