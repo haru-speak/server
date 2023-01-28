469ee3f1321e2c592586b8720ca12c872c223efe
DoooongDong <rhkswls98@kakao.com>
2023-01-28 14:59:01 +0900

    refactor: PreSignedUrlResponse 실제 파일 이름 제공하도록 수정 및 성공시 간단한 테스트 작성

--- be/src/main/java/com/example/be/core/application/dto/response/PreSignedUrlResponse.java
+++ be/src/main/java/com/example/be/core/application/dto/response/PreSignedUrlResponse.java
@@ -9,7 +9,11 @@ public class PreSignedUrlResponse {
     @Schema(type = "String", description = "파일 접근 URL, NOT NULL")
     private final String preSignedUrl;
 
-    public PreSignedUrlResponse(String preSignedUrl) {
+    @Schema(type = "String", description = "실제로 저장되는 파일 이름, NOT NULL")
+    private final String fileName;
+
+    public PreSignedUrlResponse(String preSignedUrl, String fileName) {
         this.preSignedUrl = preSignedUrl;
+        this.fileName = fileName;
     }
 }
