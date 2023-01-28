b4df05856d04270e5595ab14e2d2dd2cc2d192f7
YeonSang <leeys12181811@gmail.com>
2023-01-28 22:59:31 +0900

    feat: [#61] 스터디 전체 조회 서비스 로직 작성

--- be/src/main/java/com/example/be/core/application/StudyService.java
+++ be/src/main/java/com/example/be/core/application/StudyService.java
@@ -4,13 +4,35 @@ import com.example.be.core.application.dto.request.StudyConditionRequest;
 import com.example.be.core.application.dto.request.StudyRequest;
 import com.example.be.core.application.dto.response.StudiesResponse;
 import com.example.be.core.application.dto.response.StudyDetailResponse;
-import com.example.be.core.domain.study.StudyType;
+import com.example.be.core.application.dto.response.StudyResponse;
+import com.example.be.core.domain.study.Study;
+import com.example.be.core.repository.study.StudyCommentRepository;
+import com.example.be.core.repository.study.StudyFavoriteRepository;
+import com.example.be.core.repository.study.StudyRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 public class StudyService {
+
+  private final StudyRepository studyRepository;
+
+  private final StudyFavoriteRepository studyFavoriteRepository;
+
+  private final StudyCommentRepository studyCommentRepository;
+
+  public StudyService(StudyRepository studyRepository,
+      StudyFavoriteRepository studyFavoriteRepository,
+      StudyCommentRepository studyCommentRepository) {
+    this.studyRepository = studyRepository;
+    this.studyFavoriteRepository = studyFavoriteRepository;
+    this.studyCommentRepository = studyCommentRepository;
+  }
 
   public StudyDetailResponse create(StudyRequest studyRequest, Long memberId) {
     log.debug("[스터디 생성] StudyRequest = {}", studyRequest);
@@ -18,8 +40,27 @@ public class StudyService {
   }
 
   public StudiesResponse find(StudyConditionRequest studyConditionRequest) {
-    log.debug("스터디 조회 type = {}", studyConditionRequest.toString());
-    return null;
+    log.debug("스터디 전체 조회 type = {}", studyConditionRequest.toString());
+
+    // 임시 (아직 로그인 구현 X)
+    Long loginMemberId = 1L;
+
+    List<Study> studies = studyRepository.findAll();
+    List<StudyResponse> studyResponses = studies.stream().map(study ->
+        new StudyResponse(
+            study.getId(),
+            study.getTitle(),
+            study.getContent(),
+            study.getPosterImage(),
+            studyFavoriteRepository.countByStudyId(study.getId()),
+            studyCommentRepository.countByStudyId(study.getId()),
+            studyFavoriteRepository.findByMemberIdAndStudy(loginMemberId, study).isPresent()
+        )).collect(Collectors.toList());
+
+    return new StudiesResponse(
+        studyConditionRequest.getType(),
+        studyResponses
+    );
   }
 
   public StudyDetailResponse findById(Long studyId) {
