0684cc59807b3bbd85fd469599ce96a53184fa90
hankwanjin <49313910+HanKwanJin@users.noreply.github.com>
2023-01-28 14:38:27 +0900

    AWS S3 설정 및 파일 업로드 기능 구현과 테스트 코드 작성 (#56)
    * infra: [#42] AWS S3 gradle 의존성 추가
    
    * infra: [#42] AWS S3 설정 및 파일 업로드 API 작성
    
    * infra: [#42] AWS S3 설정 및 파일(이미지,음성) 업로드 API 작성
    
    * test: [#42] AWS S3 Mock을 이용한 업로드 테스트 코드 작성
    
    * test: [#42] AWS S3 Mock을 이용한 업로드 테스트 코드 작성
    
    * refactor: [#42] FileUploadService 어노테이션 @Service로 변경
    
    * refactor: [#42] FileUploadResponse @NotBlank 어노테이션 제거
    
    * refactor: [#42] AWS S3 Pre-Signed URL을 이용한 방식으로 파일 업로드 하도록 수정
    
    * refactor: [#42] 사용하지 않도록 바뀐 기존 upload 테스트 코드 삭제
    
    * refactor: [#42] build.gradle, 오타, 접근 지정자 수정 및 파일업로드 부분 로그 추가 작성
    
    ---------
    
    Co-authored-by: 나든 <67811880+nathan29849@users.noreply.github.com>

--- be/src/main/java/com/example/be/common/response/ResponseCodeAndMessages.java
+++ be/src/main/java/com/example/be/common/response/ResponseCodeAndMessages.java
@@ -21,6 +21,11 @@ public enum ResponseCodeAndMessages implements CodeAndMessages {
 	FIND_QUESTION_SUCCESS("S-Q001", "오늘의 문장 조회를 성공했습니다."),
 
 	/**
+	FileUpload
+	 */
+	GENERATE_IMAGE_UPLOAD_URL_SUCCESS("S-F001", "이미지 파일 업로드를 위한 URL 생성을 성공했습니다."),
+	GENERATE_VOICE_UPLOAD_URL_SUCCESS("S-F002", "음성 파일 업로드를 위한 URL 생성을 성공했습니다."),
+
 	 Member
 	 */
 	JOIN_SUCCESS("S-M001", "회원가입에 성공했습니다."),
--- be/src/main/java/com/example/be/core/web/QuestionController.java
+++ be/src/main/java/com/example/be/core/web/QuestionController.java
@@ -6,7 +6,6 @@ import com.example.be.common.response.BaseResponse;
 import com.example.be.core.application.QuestionService;
 import com.example.be.core.application.dto.response.QuestionResponse;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
