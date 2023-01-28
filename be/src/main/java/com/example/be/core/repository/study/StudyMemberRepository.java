c69bf803f2f6e3dc99ccb7f996917395bc39a046
DoooongDong <rhkswls98@kakao.com>
2023-01-28 22:22:12 +0900

    Study 생성 서비스 로직 작성

--- be/src/main/java/com/example/be/core/repository/study/StudyMemberRepository.java
+++ be/src/main/java/com/example/be/core/repository/study/StudyMemberRepository.java
@@ -1,2 +1,11 @@
-package com.example.be.core.repository.study;public class StudyMemberRepository {
+package com.example.be.core.repository.study;
+
+import com.example.be.core.domain.study.Study;
+import com.example.be.core.domain.study.StudyMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudyMemberRepository extends JpaRepository<StudyMember, Long> {
+
 }
