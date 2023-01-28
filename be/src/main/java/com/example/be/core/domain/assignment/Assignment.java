77cdfc3bda7ce55c1f3b7b4886feb90695d3a383
hankwanjin <49313910+HanKwanJin@users.noreply.github.com>
2023-01-28 20:09:48 +0900

    Study, Assignment 관련 Entity 작성 (#60)
    * feature: [#59] Study, Assignment 관련 Entity 작성
    
    * refactor: [#59] Study Entity soft delete sql 오타 수정
    
    * feature: [#59] Assignment Entity soft delete 기능 추가

---
+++ be/src/main/java/com/example/be/core/domain/assignment/Assignment.java
@@ -0,0 +1,54 @@
+package com.example.be.core.domain.assignment;
+
+import com.example.be.core.domain.BaseEntity;
+import com.example.be.core.domain.study.Study;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@Entity
+@Where(clause = "deleted = false")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE assignment SET deleted = true WHERE assignment_id = ?")
+public class Assignment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assignment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    private Study study;
+
+    private String title;
+
+    private LocalDateTime deadLine;
+
+    private String content;
+
+    @Lob
+    private String voiceRecord;
+
+    public Assignment(Study study, String title, LocalDateTime deadLine, String content,
+        String voiceRecord) {
+        this.study = study;
+        this.title = title;
+        this.deadLine = deadLine;
+        this.content = content;
+        this.voiceRecord = voiceRecord;
+    }
+}
---
+++ be/src/main/java/com/example/be/core/domain/study/StudyInterest.java
@@ -0,0 +1,39 @@
+package com.example.be.core.domain.study;
+
+import com.example.be.core.domain.BaseEntity;
+import com.example.be.core.domain.member.Member;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudyInterest extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "study_interest_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    private Study study;
+
+    public StudyInterest(Member member, Study study) {
+        this.member = member;
+        this.study = study;
+    }
+}
