a292d02eb61ba3a3ae892a4477ce27d19596f41c
DoooongDong <rhkswls98@kakao.com>
2023-01-30 01:15:04 +0900

    feature: [#71] 과제 개별 조회 서비스 로직 작성

--- be/src/main/java/com/example/be/core/application/AssignmentService.java
+++ be/src/main/java/com/example/be/core/application/AssignmentService.java
@@ -1,5 +1,6 @@
 package com.example.be.core.application;
 
+import com.example.be.common.exception.assignment.NotFoundAssignmentIdException;
 import com.example.be.common.exception.speakinglog.NotFoundMemberIdException;
 import com.example.be.common.exception.study.NotFoundStudyIdException;
 import com.example.be.core.application.dto.request.AssignmentRequest;
@@ -17,6 +18,7 @@ import com.example.be.core.repository.member.MemberRepository;
 import com.example.be.core.repository.study.StudyMemberRepository;
 import com.example.be.core.repository.study.StudyRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -98,7 +100,17 @@ public class AssignmentService {
 
   public AssignmentResponse findById(Long assignmentId) {
     log.debug("[과제 개별 조회] assignmentId = {}", assignmentId);
-    return null;
+
+    Assignment assignment = assignmentRepository.findById(assignmentId)
+        .orElseThrow(NotFoundAssignmentIdException::new);
+
+    return new AssignmentResponse(
+        assignmentId,
+        assignment.getTitle(),
+        assignment.getStudy().getId(),
+        assignment.getStudy().getTitle(),
+        assignment.getDeadLine()
+    );
   }
 
   @Transactional
