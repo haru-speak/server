9d3a5028e118c34c65792f7fdf43a7d1f4c3cd10
nathan29849 <phs960418@gmail.com>
2023-01-30 04:41:55 +0900

    feat: [#57] Login 관련 ArgumentResolver 추가 및 Interceptor 체크 로직 추가

--- be/src/main/java/com/example/be/common/config/WebConfig.java
+++ be/src/main/java/com/example/be/common/config/WebConfig.java
@@ -1,9 +1,40 @@
 package com.example.be.common.config;
 
+import com.example.be.oauth.LoginArgumentResolver;
+import com.example.be.oauth.OAuthInterceptor;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	private final OAuthInterceptor oAuthInterceptor;
+	private final LoginArgumentResolver loginArgumentResolver;
+
+	public WebConfig(OAuthInterceptor oAuthInterceptor, LoginArgumentResolver loginArgumentResolver) {
+		this.oAuthInterceptor = oAuthInterceptor;
+		this.loginArgumentResolver = loginArgumentResolver;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(oAuthInterceptor)
+			.addPathPatterns("/**")
+			.excludePathPatterns(
+				"/error/**",
+				"/oauth/**",
+				"/swagger-ui/**",
+				"/swagger-resources/**",
+				"/v3/api-docs/**",
+				"/favicon.ico"
+			);
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginArgumentResolver);
+	}
 }
