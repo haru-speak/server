2b785cc5211e4530dd300988b652fb0e29794e57
nathan29849 <phs960418@gmail.com>
2023-01-30 04:20:07 +0900

    feat: [#57] Kakao 소셜 로그인 관련 ResponseDTO 추가

---
+++ be/src/main/java/com/example/be/oauth/provider/dto/LoginResponse.java
@@ -0,0 +1,24 @@
+package com.example.be.oauth.provider.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginResponse {
+
+	private final Long memberId;
+	private final String nickname;
+	private final String profileImage;
+	private final String accessToken;
+	private final String refreshToken;
+	private final boolean newbie;
+
+	public LoginResponse(Long memberId, String nickname, String profileImage,
+		String accessToken, String refreshToken, boolean newbie) {
+		this.memberId = memberId;
+		this.nickname = nickname;
+		this.profileImage = profileImage;
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.newbie = newbie;
+	}
+}
