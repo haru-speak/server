1da342ca720ffce6c15ecf48ab4e11e559458ec7
nathan29849 <phs960418@gmail.com>
2023-01-31 10:53:56 +0900

    refactor: [#82] @Transactional 어노테이션 제거

--- be/src/test/java/com/example/be/core/application/study/StudyCreateTest.java
+++ be/src/test/java/com/example/be/core/application/study/StudyCreateTest.java
@@ -8,10 +8,8 @@ import com.example.be.core.application.dto.response.StudyDetailResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.transaction.annotation.Transactional;
 
 
-@Transactional
 @DisplayName("서비스 테스트 : Study 생성")
 class StudyCreateTest extends InitServiceTest {
 
