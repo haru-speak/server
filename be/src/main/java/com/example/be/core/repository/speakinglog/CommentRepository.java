5e0ba1452118cd4e204e7b86ad44cf533c9e4950
nathan29849 <phs960418@gmail.com>
2023-02-01 17:00:32 +0900

    refactor: [#82] Repository 어노테이션 제거

--- be/src/main/java/com/example/be/core/repository/speakinglog/CommentRepository.java
+++ be/src/main/java/com/example/be/core/repository/speakinglog/CommentRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	Integer countBySpeakingLogId(Long speakingLogId);
--- be/src/main/java/com/example/be/core/repository/speakinglog/SpeakingLogRepository.java
+++ be/src/main/java/com/example/be/core/repository/speakinglog/SpeakingLogRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SpeakingLogRepository extends JpaRepository<SpeakingLog, Long> {
 
     List<SpeakingLog> findAllByCreatedAtBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
